58e465d85d649977d4fe32fa9f7732d7af3e2419
Matthew Buckton <matthew@buckton.org>
2025-12-12 08:24:53 +1100

    add them to the startup

--- scripts/start_web_side.bat
+++ scripts/start_web_side.bat
@@ -1,2 +1,3 @@
 @echo off
 start "common-request" mvn -q -Dexec.mainClass=io.mapsmessaging.client.CommonRequestClient exec:java
+start "common-request" mvn -q -Dexec.mainClass=io.mapsmessaging.control.CentralControl exec:java
--- scripts/start_web_side.sh
+++ scripts/start_web_side.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 nohup mvn -q -Dexec.mainClass=io.mapsmessaging.client.CommonRequestClient exec:java &
+nohup mvn -q -Dexec.mainClass=io.mapsmessaging.control.CentralControl exec:java &
